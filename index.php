675226187f94e0a9f34eae050b7e05a67c35a218
allanrodriguesmachado <allan.rodrigues14@hotmail.com>
2021-07-18 19:07:33 -0300

    Project MVC - PHP -> Parte 1

--- index.php
+++ index.php
@@ -5,6 +5,15 @@ require __DIR__ . '/vendor/autoload.php';
 
 use \App\Controller\Pages\Home;
 
+$obRequest = new \App\Http\Request();
+echo "<pre>";
+print_r($obRequest);
+echo "<pre>"; exit;
+
+
+
+exit;
+
 echo Home::getHome();
 
 
